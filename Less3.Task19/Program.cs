b1dac43495303d66dceeb520172996be9664b22b
YuliyaYahshigulova <lukyashkina_da@mail.ru>
2022-11-09 22:47:37 +0700

    add solution of task19

---
+++ Less3.Task19/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите пятизначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int a, b, d, f;
+if (10000 <= n && n < 100000)
+{
+    a = n / 10000;
+    b = (n / 1000) % 10;
+    d = (n / 10) % 10;
+    f = n % 10;
+    if (a == f && b == d)
+    {
+        Console.WriteLine("Это палиндром!");
+    }
+    else
+    {
+        Console.WriteLine("Это не палиндром!");
+    }
+}
+else
+{
+    Console.WriteLine("Введено неверное число!");
+}
