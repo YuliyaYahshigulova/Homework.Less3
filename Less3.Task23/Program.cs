96cf98cd589e4fe7fe9d9da7e6777272c1c23d2f
YuliyaYahshigulova <lukyashkina_da@mail.ru>
2022-11-09 23:47:54 +0700

    add solution of task23

---
+++ Less3.Task23/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Введите любое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void Pow(int n)
+{
+    int power = 3;
+    for (int i = 1; i <= n; i++)
+    {
+        Console.WriteLine($"{i}^{power} = {Math.Pow(i, power)}");
+    }
+}
+Pow(n);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//int value = 2;
+//for (int power = 0; power <= 32; power++)
+//   Console.WriteLine($"{value}^{power} = {(long)Math.Pow(value, power)} ");
+
